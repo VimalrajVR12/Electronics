8f8f0521b2f2e4f5fe30e847ddb6f262d85a06c7
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.com>
2023-10-17 20:36:40 +0530

    added : navbar draft 1

--- electronics-website/src/components/Navbar.jsx
+++ electronics-website/src/components/Navbar.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
-import {Link} from "react-router-dom"
-import "../styles/Navbar.css"
-
+import { NavLink } from "react-router-dom";
+import styles from "../styles/Navbar.module.css"
+import {
+  homeSVG,
+  wishlistSVG,
+  cartSVG,
+  loginSVG,
+  loggedinSVG,
+  productSVG,
+} from "./SVGs";
 const Navbar = () => {
+  const isAuth = false;
   return (
-    <div style={{display: "flex", marginTop: "20px", justifyContent: "space-around"}}>
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
-      <Link to="/wishlist">WishList</Link>
-      <Link to="/cart">Cart</Link>
-      <Link to="/login">Login</Link>
+    <div className={styles.containerPC}>
+      <NavLink to="/">{homeSVG}</NavLink>
+      <div>
+        <input
+          type="text"
+          className={styles.searchInput}
+          placeholder="Search Products"
+        />
+      </div>
+      <div className={styles.optionsPC}>
+        <NavLink to="/products">{productSVG}</NavLink>
+        <NavLink to="/wishlist">{wishlistSVG}</NavLink>
+        <NavLink to="/cart">{cartSVG}</NavLink>
+        <NavLink to="/login">{isAuth ? loggedinSVG : loginSVG}</NavLink>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
--- electronics-website/src/pages/Home.jsx
+++ electronics-website/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css"
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Home Page</h1>
     </div>
   )
