976dd63f846cd337aea1460243a1f3ee06a66db4
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.com>
2023-10-20 03:37:12 +0530

    added : hamburger menu

--- electronics-website/src/components/Carousel.jsx
+++ electronics-website/src/components/Carousel.jsx
@@ -1,13 +1,11 @@
-import React,{useRef} from 'react'
+import React,{useEffect, useRef, useState} from 'react'
 import styles from "../styles/Carousel.module.css"
 import ProductContainer from './ProductContainer';
 const Carousel = ({data}) => {
   
   return (
     <div className={styles.container}>
-      {data.map((el,i) => (
-        <ProductContainer key={i} img={el.img} title={el.title} price={el.price} discountPrice={el.discountPrice} />
-      ))}
+      {data.map((el,i) => <ProductContainer key={i+el.img} img={el.img} title={el.title} price={el.price} discountPrice={el.discountPrice} />)}
     </div>
   );
 }
