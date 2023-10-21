89e39c5050ae7264d6a49e3a46c7493fc22da9c2
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.com>
2023-10-22 02:23:09 +0530

    added : first draft

--- electronics-website/src/components/Footer.jsx
+++ electronics-website/src/components/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import "../styles/Footer.css"
+import styles from "../styles/Footer.module.css"
+import { fbSVG, instaSVG, xSVG, ytSVG } from './SVGs'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className={styles.container}>
+      <div className={styles.socials}>
+        <span className={styles.fbSVG}>{fbSVG}</span>
+        <span className={styles.xSVG}>{xSVG}</span>
+        <span className={styles.instaSVG}>{instaSVG}</span>
+        <span className={styles.ytSVG}>{ytSVG}</span>
+      </div>
+      <p>Email us at <span className={styles.emailContainer}>service@saw.com</span></p>
+      <h2 className={styles.head}>Shock and Awe ©</h2>
     </div>
-  )
+  );
 }
 
 export default Footer
