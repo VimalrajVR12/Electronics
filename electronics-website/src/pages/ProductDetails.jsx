5f8a5d588f50b2a0749b4ef8b514271b3fd91306
Mrinal26 <Mrinalkumar5046@gmail.com>
2023-10-23 01:15:24 +0530

    cssimplement

--- electronics-website/src/pages/ProductDetails.jsx
+++ electronics-website/src/pages/ProductDetails.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import "../styles/ProductDetails.css"
 
-const ProductDetails = () => {
+function ProductDetails() {
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/products/${productId}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, [productId]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Product Details Page</h1>
+    <div className="product-details-container">
+      <img className="product-image" src={product.image} alt={product.name} />
+      <h1 className="product-title">{product.name.slice(0, 60)}</h1>
+      <p className="product-price">Price: ${product.price}</p>
+      <p className="product-rating">Rating: {product.rating}</p>
+      <p className="product-description">Description: {product.name}</p>
     </div>
-  )
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
+
